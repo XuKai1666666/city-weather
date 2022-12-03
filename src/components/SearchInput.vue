89d9acfc885ced81748e39b10511a3de06bd0d5d
Xukai1666666 <2684024189@qq.com>
2022-12-03 22:17:05 +0800

    axios天气数据请求

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,28 +1,34 @@
 <template>
-    <a-space direction="vertical">
-      <a-input-search
-        v-model:value="value"
-        placeholder="input search text"
-        enter-button
-        @search="onSearch"
-      />
-    </a-space>
-  </template>
-  <script lang="ts">
-  import { defineComponent, ref } from 'vue';
-  export default defineComponent({
-    setup() {
-      const value = ref<string>('');
-  
-      const onSearch = (searchValue: string) => {
-        console.log('use value', searchValue);
-        console.log('or use this.value', value.value);
-      };
-  
-      return {
-        value,
-        onSearch,
-      };
-    },
-  });
-  </script>
+  <a-space direction="vertical">
+    <a-input-search v-model:value="value" placeholder="input search text" enter-button @search="onSearch" />
+  </a-space>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import axios from 'axios'
+export default defineComponent({
+  setup() {
+    const value = ref<string>('');
+    const wheatherApiUrl: string = "http://apis.juhe.cn/simpleWeather/query/";
+    const key: string = "&key=fe96221d015c2d2ed59065fc3ed591bb";
+    const onSearch = (searchValue: string) => {
+      console.log('use value', searchValue);
+      console.log('or use this.value', value.value);
+      console.log(value.value)
+      axios.get(wheatherApiUrl + searchValue + key, {
+      }).then((res) => {
+        console.log('数据：', res);
+      }).catch((err)=>{
+        console.error(err);
+      })
+    };
+
+
+
+    return {
+      value,
+      onSearch,
+    };
+  },
+});
+</script>
