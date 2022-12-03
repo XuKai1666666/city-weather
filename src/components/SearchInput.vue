e0b766870f87022bb0de834323c91fe4f5487897
Xukai1666666 <2684024189@qq.com>
2022-12-03 23:46:58 +0800

    api地址拼合

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -9,20 +9,22 @@ import axios from 'axios'
 export default defineComponent({
   setup() {
     const value = ref<string>('');
-    const wheatherApiUrl: string = "http://apis.juhe.cn/simpleWeather/query/";
+    const wheatherApiUrl: string = "http://apis.juhe.cn/simpleWeather/query?city=";
     const key: string = "&key=fe96221d015c2d2ed59065fc3ed591bb";
     const onSearch = (searchValue: string) => {
       console.log('use value', searchValue);
       console.log('or use this.value', value.value);
       console.log(value.value)
-      axios.get(wheatherApiUrl + searchValue + key, {
+      console.log(encodeURI(searchValue))
+      console.log(wheatherApiUrl + encodeURI(searchValue) + key)
+      axios.get(wheatherApiUrl + encodeURI(searchValue) + key, {
       }).then((res) => {
         console.log('数据：', res);
-      }).catch((err)=>{
+      }).catch((err) => {
         console.error(err);
       })
     };
-
+    
 
 
     return {
