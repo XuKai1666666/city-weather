07d2c84435278e78b0834e21e54a9f17d6ce3407
Xukai1666666 <2684024189@qq.com>
2023-02-11 22:32:51 +0800

    修改监听存储函数

--- src/utils/sessionStorage.ts
+++ src/utils/sessionStorage.ts
@@ -1,34 +1,12 @@
-export default function resetSetItem(key, newVal) {
-    if (key === 'watchStorage') {
-        // 创建一个StorageEvent事件
-        var newStorageEvent = document.createEvent('StorageEvent');
-        const storage = {
-            setItem: function (k, val) {
-                sessionStorage.setItem(k, val);
-
-                // 初始化创建的事件
-                newStorageEvent.initStorageEvent('setItem', false, false, k, null, val, null, null);
-
-                // 派发对象
-                window.dispatchEvent(newStorageEvent)
-            }
-        }
-        return storage.setItem(key, newVal);
-    }
-    if (key === 'wellData') {
-        // 创建一个StorageEvent事件
-        var newStorageEvent = document.createEvent('StorageEvent');
-        const storage = {
-            setItem: function (k, val) {
-                sessionStorage.setItem(k, val);
-
-                // 初始化创建的事件
-                newStorageEvent.initStorageEvent('setItem', false, false, k, null, val, null, null);
-
-                // 派发对象
-                window.dispatchEvent(newStorageEvent)
-            }
-        }
-        return storage.setItem(key, newVal);
+function dispatchEventStroage() {
+    const signSetItem = sessionStorage.setItem
+    sessionStorage.setItem = function(key, val) {
+        let setEvent = new Event('setItemEvent')
+        setEvent.key = key
+        setEvent.newValue = val
+        window.dispatchEvent(setEvent)
+        signSetItem.apply(this, arguments)
     }
 }
+
+export default dispatchEventStroage;
